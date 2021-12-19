bf1cccb3a4bbfc7b747ca31f6ed6d4ff84013073
matthewperiut <slaincow@gmail.com>
2021-12-19 18:29:06 -0500

    Effective way to replace existing old blocks

---
+++ src/main/java/com/matthewperiut/chisel/legacy/block/ReplaceBlockState.java
@@ -0,0 +1,14 @@
+/*package com.matthewperiut.chisel.legacy.block;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.BlockState;
+
+public class ReplaceBlockState extends AbstractBlock.AbstractBlockState
+{
+    @Override
+    protected BlockState asBlockState()
+    {
+        return null;
+    }
+}
+*/
