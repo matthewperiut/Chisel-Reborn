a25ff5415a40649a009386c1d32504511b083bdd
Dan66ika77 <69496663+Danika6677@users.noreply.github.com>
2023-05-05 21:30:51 +0200

    Add 1.19.4 support

--- src/main/java/com/matthewperiut/chisel/ChiselClient.java
+++ src/main/java/com/matthewperiut/chisel/ChiselClient.java
@@ -4,6 +4,7 @@ import com.matthewperiut.chisel.block.GeneratedClientRegister;
 import com.matthewperiut.chisel.gui.ChiselScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
+import net.minecraft.client.gui.screen.ingame.HandledScreens;
 
 import static com.matthewperiut.chisel.Chisel.CHISEL_SCREEN_HANDLER;
 
@@ -12,7 +13,7 @@ public class ChiselClient implements ClientModInitializer
     @Override
     public void onInitializeClient()
     {
-        ScreenRegistry.register(CHISEL_SCREEN_HANDLER, ChiselScreen::new);
+        HandledScreens.register(CHISEL_SCREEN_HANDLER, ChiselScreen::new);
         GeneratedClientRegister.Register();
     }
 }
