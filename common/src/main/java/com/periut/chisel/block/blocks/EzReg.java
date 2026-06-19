1eed84468ff587a18eb04a7a37bea8cfa531916e
MissBismuth <194607097+MissBismuth@users.noreply.github.com>
2026-06-19 18:53:25 +0200

    fixed glass lighting

--- common/src/main/java/com/periut/chisel/block/blocks/EzReg.java
+++ common/src/main/java/com/periut/chisel/block/blocks/EzReg.java
@@ -1,6 +1,5 @@
 package com.periut.chisel.block.blocks;
 
-import com.periut.chisel.Chisel;
 import com.periut.chisel.block.ChiselGroupLookup;
 import com.periut.chisel.platform.ItemGroupHelper;
 import com.periut.chisel.platform.RegistryHelper;
@@ -14,6 +13,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.PoweredBlock;
+import net.minecraft.world.level.block.TransparentBlock;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import java.util.function.Supplier;
@@ -27,6 +27,7 @@ public class EzReg
         boolean purpur = false;
         boolean nether_brick = false;
         boolean quartz = false;
+        boolean glass = false;
 
         if (name.toLowerCase().contains("redstone"))
             redstone = true;
@@ -42,11 +43,13 @@ public class EzReg
 
         if (name.toLowerCase().contains("quartz"))
             quartz = true;
+        if (name.toLowerCase().contains("glass"))
+            glass = true;
 
         String[] individual = name.split("/", 2);
         Identifier baseBlockIdentifier = Identifier.fromNamespaceAndPath("minecraft",individual[1]);
         ChiselGroupLookup.addItemToGroup(individual[1], baseBlockIdentifier);
-        Block baseBlock = nether_brick ? Blocks.NETHER_BRICKS : (purpur ? Blocks.PURPUR_BLOCK : (quartz ? Blocks.QUARTZ_BLOCK : BuiltInRegistries.BLOCK.getValue(baseBlockIdentifier)));
+        Block baseBlock = glass ? Blocks.GLASS : (nether_brick ? Blocks.NETHER_BRICKS : (purpur ? Blocks.PURPUR_BLOCK : (quartz ? Blocks.QUARTZ_BLOCK : BuiltInRegistries.BLOCK.getValue(baseBlockIdentifier))));
         Supplier<Block> blockSupplier;
 
         Identifier block_id = Identifier.fromNamespaceAndPath("chisel", name);
@@ -57,6 +60,8 @@ public class EzReg
             blockSupplier = RegistryHelper.register(BuiltInRegistries.BLOCK, block_id, () -> new PoweredBlock(BlockBehaviour.Properties.ofFullCopy(baseBlock).setId(block_key)));
         else if (pillar)
             blockSupplier = RegistryHelper.register(BuiltInRegistries.BLOCK, block_id, () -> new RotatedPillarBlock(BlockBehaviour.Properties.ofFullCopy(baseBlock).setId(block_key)));
+        else if (glass)
+            blockSupplier = RegistryHelper.register(BuiltInRegistries.BLOCK, block_id, () -> new TransparentBlock(BlockBehaviour.Properties.ofFullCopy(baseBlock).setId(block_key)));
         else
             blockSupplier = RegistryHelper.register(BuiltInRegistries.BLOCK, block_id, () -> new Block(BlockBehaviour.Properties.ofFullCopy(baseBlock).setId(block_key)));
 
